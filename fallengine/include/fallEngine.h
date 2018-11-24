943d660bfd6889f3bb55b768f0b9e9dfa3571e58
mamg22 <45301823+mamg22@users.noreply.github.com>
2018-11-23 22:41:49 -0400

    Update fallEngine.h

--- fallengine/include/fallEngine.h
+++ fallengine/include/fallEngine.h
@@ -13,18 +13,18 @@
  *                          #     ###
  *                                #
  *                                #####
- *                          [][][][][][]
+ *                         [TTTTTTTTTTT]
  *
  *  This is fallEngine v1.0
- *  The open source Caida* game engine
+ *  The open source Caida game engine
  *  License pending
  *
- *  All classes (except Hand) can be expanded to allow new game resources and data in them such as names, sprites, etc.
+ *  All classes can be expanded to allow new game resources and data in them such as names, sprites, etc.
  *
- *  * Caida is the game in which this engine is based, it is commonly played in Venezuela, later i will include a
+ *  Caida is the game in which this engine is based, it is commonly played in Venezuela, later i will include a
  *  guide on how to play it.
  *
- *  Author: mamg22 (Marco M.)
+ *  Author: mamg22 
  *
  */
 
