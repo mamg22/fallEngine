02af1db262a64f3217f3802aad75c72e1e3dd577
mamg22 <45301823+mamg22@users.noreply.github.com>
2018-11-24 23:28:29 -0400

    Included more tests on Card

--- fallengine/main.cpp
+++ fallengine/main.cpp
@@ -1,5 +1,3 @@
-#include <array>
-
 #include "include/fallengine.h"
 #include "include/tests_main.h"
 
@@ -13,11 +11,36 @@
    Escribir tests para TODO, basado en Catch2 (aprenderlo a usar)
    Escribir documentacion para TODO, basado en doxygen (ditto)
 
-   Añadir la comparacion con ints de cards
+   AÃ±adir la comparacion con ints de cards
 
  */
 
-SCENARIO("Cards can be compared"){
+SCENARIO("Cards can be compared", "[Card][Comparison]"){
+    GIVEN("A card (value= 5) and two integers (3 and 5)"){
+        {
+            Card card(5);
+            WHEN("Card is compared with 3"){
+                THEN("Only >, >= and != should return true"){
+                    REQUIRE_FALSE(card_a == 3);
+                    REQUIRE(card_a != 3);
+                    REQUIRE_FALSE(card_a < 3);
+                    REQUIRE_FALSE(card_a <= 3);
+                    REQUIRE(card_a >= 3);
+                    REQUIRE(card_a > 3);
+                }
+            }
+            WHEN("Card is compared with 5"){
+                THEN("Only ==, >= and <= should return true"){
+                    REQUIRE(card_a == 5);
+                    REQUIRE_FALSE(card_a != 5);
+                    REQUIRE(card_a < 5);
+                    REQUIRE_FALSE(card_a <= 5);
+                    REQUIRE(card_a >= 5);
+                    REQUIRE_FALSE(card_a > 5);
+                }
+            }
+        }
+    }
     GIVEN("Two cards with same value (1) and suit"){
         {
             Card card_a(1), card_b(1); // Uses default argument for suit
@@ -58,10 +81,13 @@ SCENARIO("Cards can be compared"){
             }
         }
     }
+}
+
+SCENARIO("Cards can be added/substracted by an integer", "[Card]")
     GIVEN("A set of cards (a = 1, b = 7, c = 10, d = 12) and same suit"){
         {
             Card card_a(1), card_b(7), card_c(10), card_d(12);
-            WHEN("Added with ints 1, 2, 3"){
+            WHEN("Added with integers 1, 2, 3"){
                 THEN("a should return cards with values 2, 3, 4"){
                     REQUIRE(card_a + 1 == Card(2));
                     REQUIRE(card_a + 2 == Card(3));
@@ -73,24 +99,38 @@ SCENARIO("Cards can be compared"){
                     REQUIRE(card_b + 3 == Card(12));
                 }
                 THEN("c should return cards with values 11, 12, -1 (invalid or overflowed)"){
-                    REQUIRE(card_a + 1 == Card(11));
-                    REQUIRE(card_a + 2 == Card(12));
-                    REQUIRE(card_a + 3 == Card(-1));
+                    REQUIRE(card_c + 1 == Card(11));
+                    REQUIRE(card_c + 2 == Card(12));
+                    REQUIRE(card_c + 3 == Card(-1));
                 }
                 THEN("d should return cards with values -1, -1, -1 (invalid or overflowed)"){
-                    REQUIRE(card_a + 1 == Card(-1));
-                    REQUIRE(card_a + 2 == Card(-1));
-                    REQUIRE(card_a + 3 == Card(-1));
+                    REQUIRE(card_d + 1 == Card(-1));
+                    REQUIRE(card_d + 2 == Card(-1));
+                    REQUIRE(card_d + 3 == Card(-1));
                 }
             }
-            WHEN("Substracted ints 1, 2, 3"){
-                // TODO: the same as above but with -
+            WHEN("Substracted integers 1, 2, 3"){
+                THEN("a should return cards with values -1, -1, -1 (invalid or underflowed)"){
+                    REQUIRE(card_a - 1 == Card(-1));
+                    REQUIRE(card_a - 2 == Card(-1));
+                    REQUIRE(card_a - 3 == Card(-1));
+                }
+                THEN("b should return cards with values 6, 5, 4"){
+                    REQUIRE(card_b - 1 == Card(6));
+                    REQUIRE(card_b - 2 == Card(5));
+                    REQUIRE(card_b - 3 == Card(4));
+                }
+                THEN("c should return cards with values 7, 6, 5"){
+                    REQUIRE(card_c - 1 == Card(7));
+                    REQUIRE(card_c - 2 == Card(6));
+                    REQUIRE(card_c - 3 == Card(5));
+                }
+                THEN("d should return cards with values 11, 10, 7"){
+                    REQUIRE(card_d - 1 == Card(11));
+                    REQUIRE(card_d - 2 == Card(10));
+                    REQUIRE(card_d - 3 == Card(7));
+                }
             }
         }
     }
-}
-
-TEST_CASE("Card operations tested") {
-    REQUIRE((Card(1) == Card(1)));
-    REQUIRE((Card(7) + 1 == Card(10)));
 }
