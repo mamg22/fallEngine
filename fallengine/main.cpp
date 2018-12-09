85592dde90a8fbf5fd1f25a2a623351087c99c54
mamg22 <45301823+mamg22@users.noreply.github.com>
2018-12-08 22:15:21 -0400

    Extended main

--- fallengine/main.cpp
+++ fallengine/main.cpp
@@ -4,8 +4,6 @@
 
 #include "include/fallEngine.h"
 
-// testing the functions so the errors come out
-
 int main()
 {
     std::mt19937 eng(1234567890);
@@ -23,5 +21,14 @@ int main()
         std::cout << card.value() << ' ' << static_cast<int>(card.suit()) << '\n';
     }
     game.step();
-
+    game.step();
+    for (auto& card : game.get_table_cards()){
+        std::cout << card.value() << ' ' << static_cast<int>(card.suit()) << '\n';
+    }
+    for (auto& player : game.get_players()){
+        for (auto& card : player.get_cards()){
+            std::cout << card.value() << ' ';
+        }
+        std::cout << ' ';
+    }
 }
