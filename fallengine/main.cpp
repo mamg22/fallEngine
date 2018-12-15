8aa276550a84a22171728debadd47974b696b924
mamg22 <45301823+mamg22@users.noreply.github.com>
2018-12-14 22:39:16 -0400

    Update main.cpp

--- fallengine/main.cpp
+++ fallengine/main.cpp
@@ -1,34 +1,31 @@
 #include <iostream>
 #include <fstream>
 #include <random>
+#include <iomanip>
 
+#define private public
 #include "include/fallEngine.h"
+
+// Todo: Find WHY it doesn't deal to player nor the table, not even calling step many times works
 
 int main()
 {
+    std::cout << std::hex;
     std::mt19937 eng(1234567890);
     Game<false, Card, Player<false, Card>, Table<Card>, decltype(eng)> game(eng);
     game.add_player();
     game.add_player();
     game.add_player();
-    game.add_player();
+    // if dealt correctly, table shoudl have 4 cards in it
+    std::cout << game.get_table_cards().size();
     game.init_game();
-    for (auto& player : game.get_players()){
-        std::cout << player.id() << ' ';
-    }
-    std::cout << '\n';
-    for (auto& card : game.get_table_cards()){
-        std::cout << card.value() << ' ' << static_cast<int>(card.suit()) << '\n';
-    }
+    std::cout << game.get_table_cards().size();
     game.step();
-    game.step();
-    for (auto& card : game.get_table_cards()){
-        std::cout << card.value() << ' ' << static_cast<int>(card.suit()) << '\n';
-    }
-    for (auto& player : game.get_players()){
-        for (auto& card : player.get_cards()){
-            std::cout << card.value() << ' ';
-        }
-        std::cout << ' ';
-    }
+    std::cout << game.get_table_cards().size();
+        game.step();
+    std::cout << game.get_table_cards().size();
+        game.step();
+    std::cout << game.get_table_cards().size();
+    // Should return 3
+    std::cout << game.current_player().get_cards().size();
 }
