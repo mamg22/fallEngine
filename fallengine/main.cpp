c20424ce927e1029e42fd97fdf6875d251d40b31
mamg22 <45301823+mamg22@users.noreply.github.com>
2018-12-21 23:07:26 -0400

    Add more debug  output

--- fallengine/main.cpp
+++ fallengine/main.cpp
@@ -10,14 +10,18 @@
 void report_round(int& round, auto& game)
 {
     std::cout << "Round: " << round++ << "===============================\n" <<
-    "CP: " << game.current_player().id() << " DP: " << game.dealer().id() << '\n' ;
+    "CP: " << game.current_player().id() << " DP: " << game.dealer().id() << " BC: " << game.best_combo_p().id() << '\n';
 }
 
 int main()
 {
     int round = 0;
-    std::mt19937 eng(1234567890);
-    Game<false, Card, Player<false, Card>, Table<Card>, decltype(eng)> game(eng);
+    std::cout << "SEED: ";
+    int seed = 0;
+    std::cin >> seed;
+    std::cout << '\n';
+    std::mt19937 eng(seed);
+    Game<false, Card, Player<false, Card>, Table<Card>, decltype(eng)> game(eng, Combo::Casa_grande);
     game.add_player();
     game.add_player();
     game.add_player();
@@ -34,7 +38,7 @@ int main()
         std::cout << card.value() << ' ' << static_cast<int>(card.suit()) << '\n';
     }
     for (auto& player : game.get_players()){
-        std::cout << "Player: " << player.id() << '\n';
+        std::cout << "Player: " << player.id() << " S: " << player.get_score() << "  Combo:" << player.get_combo_name() << '\n';
         for (auto& card : player.get_cards()){
             std::cout << card.value() << ' ' << static_cast<int>(card.suit()) << '\n';
         }
@@ -50,11 +54,11 @@ int main()
         std::cout << card.value() << ' ' << static_cast<int>(card.suit()) << '\n';
     }
     for (auto& player : game.get_players()){
-        std::cout << "Player: " << player.id() << '\n';
+        std::cout << "Player: " << player.id() << " S: " << player.get_score() << "  Combo:" << player.get_combo_name() << '\n';
         for (auto& card : player.get_cards()){
             std::cout << card.value() << ' ' << static_cast<int>(card.suit()) << '\n';
         }
     }
     // TODO: now test the player's methods, and continue the game, and make the interpeter for the small scripting
-    // language that allows game automation (defined in fallengine/fescript/commands.txt)
+    // language that allows game automation (defined in fallengine/fescript/commands.txt
 }
