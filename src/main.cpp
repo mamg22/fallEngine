96997722d0b40409eaebea1a670f10b252d8dcc9
mamg22 <45301823+mamg22@users.noreply.github.com>
2019-02-05 22:09:52 -0400

    Add license and readme, push to github

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,6 @@
 #include "../include/fallengine.h"
 #include "../include/fespar/fespar.h"
 
-// TODO: Reparar Table::take_card, que eleimina cosas basado en la posicion y no en la carte en si (Semilla 99, eliminar 2, 3. elimina 2, 7)
-
 template<class Game_type>
 void report_round(int& round, Game_type& game)
 {
